9b143ed24e2e8249e941792713bd455212702412
Ethan Jones <joneetha@oregonstate.edu>
2020-02-08 22:16:20 -0800

    add color output to example script

--- example.sh
+++ example.sh
@@ -1,24 +1,33 @@
+trap "exit" INT
+
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 aws s3 rm --recursive s3://cs493-joneetha-music
 
-echo "Bucket is empty after reset:"
+printf "${BLUE}Bucket is empty after reset:${NC}\n"
 aws s3 ls s3://cs493-joneetha-music
-echo
+printf ""
 
-echo "Uploading a song:"
+printf "${BLUE}Uploading a song:${NC}\n"
 python3 app.py upload_song cs493-joneetha-music deafheaven/sunbather/01_dream_house 01_dream_house sunbather deafheaven --profile=s3role
-echo "Listing contents of album:"
+printf "${BLUE}Listing contents of album:${NC}\n"
 aws s3 ls s3://cs493-joneetha-music/deafheaven/sunbather/ --profile=s3role
-echo
+printf "${BLUE}Renaming the song:${NC}\n"
+python3 app.py rename_song cs493-joneetha-music xXxDrEaM_HoUsExXx 01_dream_house sunbather deafheaven --profile=s3role
+printf "${BLUE}Listing contents of album:${NC}\n"
+aws s3 ls s3://cs493-joneetha-music/deafheaven/sunbather/ --profile=s3role
+printf ""
 
-echo "Uploading an album:"
+printf "${BLUE}Uploading an album:${NC}\n"
 python3 app.py upload_album cs493-joneetha-music deafheaven/sunbather sunbather deafheaven --profile=s3role
-echo "Listing contents of album:"
+printf "${BLUE}Listing contents of album:${NC}\n"
 aws s3 ls s3://cs493-joneetha-music/deafheaven/sunbather/ --profile=s3role
-echo
+printf ""
 
-echo "Uploading an artist:"
+printf "${BLUE}Uploading an artist:${NC}\n"
 python3 app.py upload_artist cs493-joneetha-music have_a_nice_life have_a_nice_life --profile=s3role
-echo "Listing albums by artist:"
+printf "${BLUE}Listing albums by artist:${NC}\n"
 aws s3 ls s3://cs493-joneetha-music/have_a_nice_life/ --profile=s3role
-echo "Listing contents of album:"
+printf "${BLUE}Listing contents of album:${NC}\n"
 aws s3 ls s3://cs493-joneetha-music/have_a_nice_life/deathconsciousness/ --profile=s3role
