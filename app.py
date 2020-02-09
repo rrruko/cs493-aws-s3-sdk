b2c8085eba600f45ff30dffea6b86081af78785b
Ethan Jones <joneetha@oregonstate.edu>
2020-02-08 16:07:34 -0800

    make uploads work

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """AWS album upload thingy.
 
 Usage:
-  app.py upload_album <bucket_name> <path> <name>
+  app.py upload_album <bucket_name> <path> <album_name> <artist_name>
   app.py upload_artist <bucket_name> <path> <name>
 
 Options:
@@ -12,34 +12,60 @@ import boto3
 from docopt import docopt
 import os
 
-bucket_name = None
+class Logger():
+  def __init__(self):
+    self.indent_level = 0
 
-def upload_song(artist_name, song_name, data):
-  s3 = boto3.resource('s3')
-  s3.Bucket(artist_name).put_object(Key=song_name, Body=data)
+  def log(self, msg):
+    print(' ' * self.indent_level + msg)
 
-def upload_album(path, name):
-  for song in os.listdir(path):
-    song_path = path + '/' + song
-    if os.path.isfile(song_path):
-      print(f'found file {song}')
-      with open(song_path, 'rb') as song_bytes:
-        upload_song(
-          artist_name=bucket_name,
-          song_name=song,
-          data=song_bytes.read())
+  def indent(self):
+    self.indent_level += 2
 
-def upload_artist(path, name):
-  pass
+  def outdent(self):
+    self.indent_level -= 2
+    self.indent_level = max(self.indent_level, 0)
+
+class App():
+  def __init__(self, bucket_name):
+    self.bucket_name = bucket_name
+    self.logger = Logger()
+
+  def upload_song(self, artist_name, album_name, song_name, data):
+    s3 = boto3.resource('s3')
+    path = artist_name + '/' + album_name + '/' + song_name
+    self.logger.log(f'uploading {song_name} at {path}')
+    s3.Bucket(self.bucket_name).put_object(Key=path, Body=data)
+
+  def upload_album(self, path, album_name, artist_name):
+    self.logger.log(f'album: {album_name}')
+    for song in os.listdir(path):
+      self.logger.indent()
+      song_path = path + '/' + song
+      if os.path.isfile(song_path):
+        with open(song_path, 'rb') as song_bytes:
+          self.upload_song(
+            artist_name=artist_name,
+            album_name=album_name,
+            song_name=song,
+            data=song_bytes.read())
+      self.logger.outdent()
+
+  def upload_artist(self, path, name):
+    print(f'artist: {name}')
+    for album in os.listdir(path):
+      self.logger.indent()
+      self.upload_album(path + '/' + album, album, name)
+      self.logger.outdent()
 
 def main():
   arguments = docopt(__doc__)
   print(arguments)
-  bucket_name = arguments['<bucket_name>']
+  app = App(arguments['<bucket_name>'])
   if arguments['upload_album']:
-    upload_album(arguments['<path>'], arguments['<name>'])
+    app.upload_album(arguments['<path>'], arguments['<album_name>'], arguments['<artist_name>'])
   elif arguments['upload_artist']:
-    upload_artist(arguments['<path>'], arguments['<name>'])
+    app.upload_artist(arguments['<path>'], arguments['<name>'])
 
 if __name__ == '__main__':
   main()
