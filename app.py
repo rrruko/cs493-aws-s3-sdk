f603d919dd781d187d28700658e96786ecc05afe
Ethan Jones <joneetha@oregonstate.edu>
2020-02-22 12:48:15 -0800

    sync ddb table with s3 bucket

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ Usage:
   app.py upload_song <bucket_name> <path> <song_name> <album_name> <artist_name> [--profile=<profile_name>]
   app.py rename_song <bucket_name> <new_name> <song_name> <album_name> <artist_name> [--profile=<profile_name>]
   app.py upload_album <bucket_name> <path> <album_name> <artist_name> [--profile=<profile_name>]
-  app.py upload_artist <bucket_name> <path> <name> [--profile=<profile_name>]
+  app.py upload_artist <bucket_name> <path> <name> [--profile=<profile_name>] --genre=<genre_name>
 
 Options:
   -h --help                 Show this screen.
   --profile=<profile_name>  Use a profile besides the default one.
+  --genre=<genre_name>      Specify genre for an artist.
 """
 
 import boto3
@@ -39,6 +40,8 @@ class App():
       self.profile = 'default'
     session = boto3.Session(profile_name=self.profile)
     self.s3 = session.resource('s3')
+    self.ddb = session.resource('dynamodb').Table('music')
+
 
   def upload_song(self, artist_name, album_name, song_name, song_path):
     with open(song_path, 'rb') as song_bytes:
@@ -46,6 +49,13 @@ class App():
       path = artist_name + '/' + album_name + '/' + song_name
       self.logger.log(f'uploading {song_name} at {path}')
       self.s3.Bucket(self.bucket_name).put_object(Key=path, Body=data)
+      self.ddb.put_item(
+        TableName='music',
+        Item={
+          'PK': album_name,
+          'SK': song_name
+        }
+      )
 
   def upload_album(self, path, album_name, artist_name):
     self.logger.log(f'album: {album_name}')
@@ -59,13 +69,35 @@ class App():
           song_name=song,
           song_path=song_path)
       self.logger.outdent()
+    self.ddb.put_item(
+      TableName='music',
+      Item={
+        'PK': artist_name,
+        'SK': album_name
+      }
+    )
 
-  def upload_artist(self, path, name):
+  def upload_artist(self, path, name, genre):
     print(f'artist: {name}')
     for album in os.listdir(path):
       self.logger.indent()
       self.upload_album(path + '/' + album, album, name)
       self.logger.outdent()
+    self.ddb.put_item(
+      TableName='music',
+      Item={
+        'PK': genre,
+        'SK': name
+      }
+    )
+    self.ddb.put_item(
+      TableName='music',
+      Item={
+        'PK': 'genre',
+        'SK': genre
+      }
+    )
+
 
   def rename_song(self, new_name, song_name, album_name, artist_name):
     path = artist_name + '/' + album_name + '/' + song_name
@@ -74,6 +106,20 @@ class App():
     print(source_obj)
     self.s3.Object(self.bucket_name, new_path).copy_from(CopySource=source_obj)
     self.s3.Object(self.bucket_name, path).delete()
+    self.ddb.delete_item(
+      TableName='music',
+      Key={
+        'PK': album_name,
+        'SK': song_name
+      }
+    )
+    self.ddb.put_item(
+      TableName='music',
+      Item={
+        'PK': album_name,
+        'SK': new_name
+      }
+    )
 
 def main():
   arguments = docopt(__doc__)
@@ -81,7 +127,7 @@ def main():
   if arguments['upload_album']:
     app.upload_album(arguments['<path>'], arguments['<album_name>'], arguments['<artist_name>'])
   elif arguments['upload_artist']:
-    app.upload_artist(arguments['<path>'], arguments['<name>'])
+    app.upload_artist(arguments['<path>'], arguments['<name>'], arguments['--genre'])
   elif arguments['upload_song']:
     app.upload_song(
       artist_name=arguments['<artist_name>'],
